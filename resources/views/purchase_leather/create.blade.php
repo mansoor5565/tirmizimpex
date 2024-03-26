68158388dd0e4a8947bdb9fcd479fbeeffe7eef0
mansoor5565 <mansoor.snt@gmail.com>
2024-03-26 12:58:24 -0700

    26-3-2024-mansoor-3

--- resources/views/purchase_leather/create.blade.php
+++ resources/views/purchase_leather/create.blade.php
@@ -10,7 +10,20 @@
                 <li class="breadcrumb-item active">Layouts</li>
             </ol>
         </nav>
-    </div><!-- End Page Title -->
+    </div>
+    <style>
+        .leather-tag {
+            text-decoration: none;
+            color: #4154f1;
+            cursor: pointer;
+        }
+
+        .leather-tag.disabled {
+            color: gray;
+            cursor: text;
+        }
+    </style>
+    <!-- End Page Title -->
     <section class="section">
         <div class="row">
             <form action="/purchase_leather" method="POST" class="row g-3">
@@ -22,17 +35,14 @@
 
                             <div class="row g-3">
                                 <div class="col-12">
-
-                                    <label for="leatherSelect" class="form-label">Choose Leather</label>
-                                    <select class="form-select mb-2" id="leatherSelect" name="leather[]" multiple>
-                                        @foreach ($leathers_color as $leather_color)
-                                            <option value="{{ $leather_color->id }}"
-                                                data-quantity="{{ $leather_color->quantity }}">
-                                                {{ $leather_color->leathers->type . ' ' . $leather_color->color }}</option>
-                                        @endforeach
-                                    </select>
+                                    @foreach ($leathers_color as $leather_color)
+                                        <a value="{{ $leather_color->id }}" class="leather-tag"
+                                            data-leather_color_id="{{ $leather_color->id }}"
+                                            onclick="show(this,{{ $leather_color->id }},'{{ $leather_color->leathers->type . ' ' . $leather_color->color }}')">
+                                            {{ $leather_color->leathers->type . ' ' . $leather_color->color }}</a> <br>
+                                    @endforeach
                                     <span class='text-danger'>
-                                        @error('leather')
+                                        @error('purchase')
                                             {{ $message }}
                                         @enderror
                                     </span>
@@ -70,19 +80,8 @@
                 <div class="col-lg-6">
                     <div class="card">
                         <div class="card-body">
-                            <div class="row g-3">
+                            <div class="row g-3" id="purchase-cost-quantity-option-div">
 
-                                @foreach ($leatherModels as $model)
-                                    <div class="col-12">
-                                        <div class="leather-model" id="model_{{ $model->id }}">
-                                            <span class="model-label"
-                                                data-model-id="{{ $model->id }}">{{ $model->type }}</span>
-                                        </div>
-                                        <input type="hidden" name="models[{{ $model->id }}][quantity]"
-                                            class="quantity-input">
-                                        <input type="hidden" name="models[{{ $model->id }}][cost]" class="cost-input">
-                                    </div>
-                                @endforeach
                             </div>
                         </div>
                     </div>
@@ -90,62 +89,43 @@
             </form>
         </div>
     </section>
+    <script>
+        let selected_leather_color_id = [];
+        modelDiv = $('#purchase-cost-quantity-option-div');
 
-    {{-- <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            const selectLeather = document.getElementById('leatherSelect');
-            const selectedLeatherContainer = document.querySelector('.selected-leather-container');
-            let selectedLeather = []; // Array to store selected leather
-    
-            // Add event listener to each option element
-            selectLeather.querySelectorAll('option').forEach(option => {
-                option.addEventListener('click', function() {
-                    const leatherName = option.textContent;
-                    const leatherId = option.value;
-                    const leatherQuantity = option.dataset.quantity;
-                    const index = selectedLeather.findIndex(leather => leather.id === leatherId);
-    
-                    if (option.selected && index === -1) {
-                        selectedLeather.push({
-                            id: leatherId,
-                            name: leatherName,
-                            quantity: leatherQuantity,
-                            cost: '', // Initialize cost as empty string
-                            vendor: '' // Initialize vendor as empty string
-                        });
-                    } else if (!option.selected && index !== -1) {
-                        selectedLeather.splice(index, 1);
-                    }
-    
-                    updateSelectedLeather();
-                });
-            });
-    
-            function updateSelectedLeather() {
-                selectedLeatherContainer.innerHTML = ''; // Clear the container
-    
-                selectedLeather.forEach(leather => {
-                    const leatherHTML = `
-                        <div class="selected-leather" data-id="${leather.id}">
-                            ${leather.name}: 
-                            <input type="number" name="leather_quantities[]" class="form-control leather-quantity" placeholder="Quantity" value="${leather.quantity}">
-                            <input type="number" name="leather_costs[]" class="form-control leather-cost" placeholder="Cost" value="${leather.cost}">
-                            <button type="button" class="btn btn-danger cancel-btn" onclick="cancelLeatherSelection(this)"><i class="bi bi-trash"></i></button>
-                        </div>
-                    `;
-                    selectedLeatherContainer.innerHTML += leatherHTML;
-                });
+        function show(element, leather_color_id, leather_name) {
+            if (selected_leather_color_id.includes(leather_color_id)) {
+                swal("Already Selected!", "Already Selected", "warning");
+                return;
             }
-    
-            window.cancelLeatherSelection = function(buttonElement) {
-                const selectedLeatherDiv = buttonElement.parentElement;
-                const leatherId = selectedLeatherDiv.dataset.id;
-                const index = selectedLeather.findIndex(leather => leather.id === leatherId);
-                if (index !== -1) {
-                    selectedLeather.splice(index, 1);
-                    selectedLeatherDiv.remove(); // Remove the selected leather from the container
-                }
-            };
-        });
-    </script> --}}
+
+            selected_leather_color_id.push(leather_color_id);
+            var index = selected_leather_color_id.indexOf(leather_color_id);
+            const optionsDiv = $('<div>').addClass('options').addClass('col-12');
+
+            const leatherHTML = `
+                        <div class="selected-leather col-12" data-id="${leather_color_id}">
+                            ${leather_name}: 
+                            <input type="number" name="purchase[${leather_color_id}][leather_color_id]" class="form-control d-none leather-quantity mb-2 mt-2" value='${leather_color_id}'>
+                            <input type="number" name="purchase[${leather_color_id}][quantity]" class="form-control leather-quantity mb-2 mt-2" placeholder="Quantity">
+                            <input type="number" name="purchase[${leather_color_id}][cost]" class="form-control leather-cost mb-2 mt-2" placeholder="Cost">
+                            <button type="button" class="btn btn-danger cancel-btn" onclick="cancelLeatherSelection(this, '${leather_color_id}')"><i class="bi bi-trash"></i></button>
+                        </div>
+                `;
+            modelDiv.append(leatherHTML);
+            $(element).addClass('disabled');
+        }
+
+        function cancelLeatherSelection(button, leather_color_id) {
+            var selectedLeatherDiv = $(button).closest('.selected-leather');
+            var leatherId = selectedLeatherDiv.data('id');
+
+            selectedLeatherDiv.remove();
+
+            var index = selected_leather_color_id.indexOf(leatherId);
+            selected_leather_color_id.splice(index, 1);
+            $(`a[data-leather_color_id="${leather_color_id}"]`).removeClass('disabled');
+        }
+    </script>
+    <script></script>
 @endsection
--- resources/views/purchase_leather/view.blade.php
+++ resources/views/purchase_leather/view.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="pagetitle">
         <h1>Purchase Leather Table</h1>
-        
+
     </div><!-- End Page Title -->
     <section class="section">
         <div class="row">
@@ -16,19 +16,19 @@
                             <a href="/purchase_leather/create" class="btn btn-primary btn-custom">Create Purchase Leather</a>
                             <style>
                                 .btn-custom {
-                                     height: 40px; /* Set the desired height */
-                                     min-width: 120px; /* Set the desired minimum width */
+                                    height: 40px;
+                                    /* Set the desired height */
+                                    min-width: 120px;
+                                    /* Set the desired minimum width */
                                     /* Adjust padding to fit the text properly */
-                                    }
+                                }
                             </style>
                         </div>
                         <!-- Table with stripped rows -->
                         <table class="table datatable">
                             <thead>
                                 <tr>
-                                    <th>Leather Color</th>
-                                    <th>Quantity</th>
-                                    <th>Cost/Unit</th>
+                                    <th>Purchase Id</th>
                                     <th>Total Cost</th>
                                     <th>Vendor</th>
                                     <th data-type="date" data-format="YYYY/DD/MM">Created Date</th>
@@ -36,24 +36,25 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                @foreach ($purchase_leather_color as $purchase_leather_colors)
+                                @foreach ($purchase_leather_colors as $purchase_leather_color)
                                     <tr>
-
-                                        <td>{{$purchase_leather_colors->leatherColors->leathers->type." ".$purchase_leather_colors->leatherColors->color ." ".$purchase_leather_colors->leather_color_id }}</td>
-                                        <td>{{$purchase_leather_colors->quantity }}</td>
-                                        <td>{{ $purchase_leather_colors->cost_per_unit }}</td>
-                                        <td>{{ $purchase_leather_colors->purchaseleathers->total_cost }}</td>
-                                        <td>{{ $purchase_leather_colors->purchaseleathers->leathervendors->name ." ".$purchase_leather_colors->purchaseleathers->leather_vendor_id }}</td>
-                                        <td>{{  $purchase_leather_colors->created_at->diffForHumans() }}</td>
                                         <td>
-                                            <a href="/purchase_leather/show/{{$purchase_leather_colors->id}}" class="action-btn btn btn-success mr-2">
+                                            {{ $purchase_leather_color->id }}
+                                        </td>
+                                        <td>{{ $purchase_leather_color->total_cost }}</td>
+                                        <td>{{ $purchase_leather_color->leathervendors->name }}</td>
+                                        <td>{{ $purchase_leather_color->created_at->diffForHumans() }}</td>
+                                        <td>
+                                            <a href="/purchase_leather/show/{{ $purchase_leather_color->id }}"
+                                                class="action-btn btn btn-success mr-2">
                                                 <i class="bi bi-eye"></i> <span>View</span>
                                             </a>
-                                            <a href="/purchase_leather/edit/{{ $purchase_leather_colors->id }}"
+                                            <a href="/purchase_leather/edit/{{ $purchase_leather_color->id }}"
                                                 class="action-btn btn btn-warning mr-2 text-white">
                                                 <i class="bi bi-pencil-square"></i> <span>Edit</span>
                                             </a>
-                                            <a href="/purchase_leather/delete/{{ $purchase_leather_colors->id }}"
+                                            <a href="javascript:void(0)"
+                                                onclick="deletePurchaseLeather({{ $purchase_leather_color->id }})"
                                                 class="action-btn btn btn-danger">
                                                 <i class="bi bi-trash"></i> <span>Delete</span>
                                             </a>
@@ -62,7 +63,7 @@
                                 @endforeach
                             </tbody>
                         </table>
-                    
+
 
                     </div>
                 </div>
@@ -71,36 +72,36 @@
         </div>
     </section>
     @push('script')
-        @if(session()->has('submitSuccess'))
+        @if (session()->has('submitSuccess'))
             <script>
                 swal("Saved Successfully!", "", "success");
             </script>
         @endif
-        @if(session()->has('updateSuccess'))
+        @if (session()->has('updateSuccess'))
             <script>
                 swal("Updated Successfully!", "", "success");
             </script>
         @endif
-        @if(session()->has('DeleteSuccess'))
+        @if (session()->has('DeleteSuccess'))
             <script>
                 swal("Deleted!", "Purchase leather has been deleted successfully.", "success");
             </script>
         @endif
-        
+
         <script>
             function deletePurchaseLeather(id) {
                 swal({
-                    title: "Are you sure?",
-                    text: "Once deleted, you will not be able to recover this purchase leather!",
-                    icon: "warning",
-                    buttons: true,
-                    dangerMode: true,
-                })
-                .then((willDelete) => {
-                    if (willDelete) {
-                        window.location.href = '/purchase_leather/delete/' + id;
-                    }
-                });
+                        title: "Are you sure?",
+                        text: "Once deleted, you will not be able to recover this purchase leather!",
+                        icon: "warning",
+                        buttons: true,
+                        dangerMode: true,
+                    })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            window.location.href = '/purchase_leather/delete/' + id;
+                        }
+                    });
             }
         </script>
     @endpush
